80d8d37435fc7f6443c34eaf4f176d3fc7d6151b
Guntur Amarnadh <guntur.amarnadh@gmail.com>
2026-01-21 01:11:07 +0530

    Update SpaForwardController.java

--- src/main/java/in/amar/billingsoftware/controller/SpaForwardController.java
+++ src/main/java/in/amar/billingsoftware/controller/SpaForwardController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SpaForwardController {
 
-    @RequestMapping(value = "/{path:[^\\.]*}")
+    @RequestMapping(value = {
+            "/", 
+            "/login",
+            "/dashboard",
+            "/**/{path:[^\\.]*}"
+    })
     public String forward() {
         return "forward:/index.html";
     }
 }
+
