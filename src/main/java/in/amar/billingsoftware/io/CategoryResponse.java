81249fff605df204035bca7372fb9629ed8e8ed5
Guntur Amarnadh <guntur.amarnadh@gmail.com>
2026-01-21 18:49:41 +0530

    Update CategoryResponse.java

--- src/main/java/in/amar/billingsoftware/io/CategoryResponse.java
+++ src/main/java/in/amar/billingsoftware/io/CategoryResponse.java
@@ -12,7 +12,6 @@ public class CategoryResponse {
     private String categoryId;
     private String name;
     private String description;
-    private String bgColor;
     private Timestamp createdAt;
     private Timestamp updatedAt;
     private String imgUrl;
