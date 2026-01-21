9d4b48a962c509866aaa8a96ff3433222c8bc57a
Guntur Amarnadh <guntur.amarnadh@gmail.com>
2026-01-21 09:56:06 +0530

    Create SpaErrorController.java

---
+++ src/main/java/in/amar/billingsoftware/controller/SpaErrorController.java
@@ -0,0 +1,13 @@
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SpaErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        return "forward:/index.html";
+    }
+}
