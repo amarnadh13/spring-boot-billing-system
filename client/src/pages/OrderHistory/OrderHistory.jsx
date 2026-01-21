128fe615d8c6eb31c4497a623ba96ae23953cc8b
Guntur Amarnadh <guntur.amarnadh@gmail.com>
2026-01-21 20:22:46 +0530

    Update OrderHistory.jsx

--- client/src/pages/OrderHistory/OrderHistory.jsx
+++ client/src/pages/OrderHistory/OrderHistory.jsx
@@ -4,7 +4,7 @@ import {latestOrders} from "../../Service/OrderService.js";
 
 const OrderHistory = () => {
     const [orders, setOrders] = useState([]);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchOrders = async () => {
