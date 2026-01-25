8dac0ae94e2588748db7a5c455eb74cabd9372ec
Guntur Amarnadh <guntur.amarnadh@gmail.com>
2026-01-25 21:20:32 +0530

    Update Menubar.jsx

--- client/src/components/Menubar/Menubar.jsx
+++ client/src/components/Menubar/Menubar.jsx
@@ -1,6 +1,6 @@
 import './Menubar.css';
 import {Link, useLocation, useNavigate} from 'react-router-dom';
-import {useContext} from "react";
+import {useContext, useState, useEffect} from "react";
 import {AppContext} from "../../context/AppContext.jsx";
 import {assets} from "../../assets/assets.js";
 
@@ -8,26 +8,34 @@ const Menubar = () => {
     const navigate = useNavigate();
     const location = useLocation();
     const {logout, auth} = useContext(AppContext);
+    const [open, setOpen] = useState(false);
+
     const handleLogout = () => {
         logout();
         navigate("/login", {replace: true});
     }
+
+    useEffect(() => {
+        setOpen(false);
+        document.body.style.overflow = "auto";
+    }, [location.pathname]);
 
     const isActive = (path) => {
         return location.pathname === path;
     }
 
     const isAdmin = auth.role === "ROLE_ADMIN";
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-2">
-            <a className="navbar-brand" href="#">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/dashboard">
                 <img src={assets.system} alt="Logo" height="40"/>
-            </a>
+            </Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setOpen(!open)}>
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse p-2" id="navbarNav">
+            <div className={`collapse navbar-collapse p-2 ${open ? "show" : ""}`} id="navbarNav">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                         <Link className={`nav-link ${isActive('/dashboard') ? 'fw-bold text-warning' : ''}`} to="/dashboard">Dashboard</Link>
